20dcce021b3d6ce8eb8198a27978dba21f440584
“Jack <w.thongsoi@gmail.com>
2025-09-19 08:42:13 +0700

    updates

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:task_app/main.dart';
+import 'package:frontend/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
